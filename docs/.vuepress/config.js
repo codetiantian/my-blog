81a3cf276b0a10d4451a0c3c2190a828a6315cc2
cuibingbing <cuibingbing@daojia-inc.com>
2024-11-08 18:56:10 +0800

    部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  base: '/my-blog/',
   bundler: viteBundler(),
   theme: defaultTheme({
     logo: 'logo.png',
